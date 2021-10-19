e2c1fc1016b3f6f881c0e81dfb41317a308a6c7f
afolabiopakunle <opakunleafolabi@gmail.com>
2021-10-19 09:13:40 +0100

    saved

---
+++ src/app/second/second.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-second',
+  styleUrls: ['./second.component.css'],
+  templateUrl: './second.component.html'
+})
+
+export class SecondComponent implements OnInit {
+
+  ngOnInit() {
+    
+  }
+}
