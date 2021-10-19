cc18e3972fe7e23ae7d0babcdda12fed79ae9e88
afolabiopakunle <opakunleafolabi@gmail.com>
2021-10-19 09:06:12 +0100

    first works

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+
+@Component({
+  selector: 'app-first',
+  styleUrls: ['./first.component.css'],
+  templateUrl: './first.component.html'
+})
+
+export class FirstComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
+}
